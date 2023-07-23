378052ea0121bf9513977f6b48d8ea3dec6017ff
Franky So <frankyso.mail@gmail.com>
2023-07-23 09:32:31 +0000

    update rule expression

--- src/Rules/DateExpressionValidator.php
+++ src/Rules/DateExpressionValidator.php
@@ -12,25 +12,8 @@ class DateExpressionValidator implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (preg_match('/^\S.*\s.*\S$/', $value)) {
-            $fail('The :attribute cannot contain spaces.');
-
-            return;
-        }
-
-        $pattern = '/(\d+[a-z]+)/i';
-        preg_match_all($pattern, $value, $matches);
-
-        $intervals = $matches[0];
-
-        foreach ($intervals as $interval) {
-            $amount = (int) filter_var($interval, FILTER_SANITIZE_NUMBER_INT);
-            $unit = str_replace($amount, '', $interval);
-
-            // Check if unit is valid
-            if (! in_array($unit, ['y', 'mo', 'w', 'd', 'h', 'm', 's'])) {
-                $fail("The :attribute must contain 'y', 'mo', 'd', 'h', 'm', or 's' only.");
-            }
+        if (!preg_match('/^(?:(\d+[ymowdhs])+|(\d+y)?(\d+mo)?)(\d+w)?(\d+d)?(\d+h)?(\d+m)?(\d+s)?$/', $value)) {
+            $fail('The :attribute expression not valid. ex: 1 day 12 hours = 1d12h');
         }
     }
 }
